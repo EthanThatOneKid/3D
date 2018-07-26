80904ee75a9088aafec7662de5e0ac774a4f5870
Ethan <ethan.r.davidson@gmail.com>
2018-07-26 13:30:19 -0700

    Finished Working Demo for p5js-test example

--- examples/p5js-test/scripts/sketch.js
+++ examples/p5js-test/scripts/sketch.js
@@ -1,15 +1,26 @@
 // Refer to the following resources
 // \_ https://p5js.org/reference/#group-Shape
 
-let cnv;
+let cnv, shape = "box";
 
 function setup() {
   cnv = createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
-  background(sin(frameCount/100)*127+127);
+  background(sin(frameCount / 100) * 127 + 127);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  torus(50, 15);
+  rotateZ(frameCount * 0.01);
+  normalMaterial();
+  switch (shape) {
+    case "box": box(50); break;
+    case "cone": cone(40, 70); break;
+    case "cylinder": cylinder(20, 50); break;
+    case "sphere": sphere(40); break;
+    case "torus": torus(50, 15); break;
+    default: plane(50, 50);
+  }
 }
+
+let changeShape = () => shape = $('#shape-input').find(":selected").text();
