9cd2216fefed57cee79290f51f930f7b6bee861b
Ethan <ethan.r.davidson@gmail.com>
2018-08-01 14:44:03 -0700

    Removed unneccessary files and cleaned up torus class file

--- examples/threejs-test/scripts/torus.js
+++ examples/threejs-test/scripts/torus.js
@@ -3,27 +3,35 @@
 ** r = radius of tube
 ** detail = amount of vertices in the output array
 */
-function torus(R, r, detail = 100) {
-  // empty list
-  let vertices = [];
 
-  // iterating through all the thetas
-  for (let radian1 = 0; radian1 <= Math.PI * 2; radian1 += Math.PI * 2 / detail) {
-    // iterating through all the phis
-    for (let radian2 = 0; radian2 <= Math.PI * 2; radian2 += Math.PI * 2 / detail * 10) {
-      // appending list with a vertex object
-      vertices.push(getVertex(radian1, radian2));
-    }
+class Torus {
+  constructor(R, r, detail = 32) {
+    this.radius = R;
+    this.tubeRadius = r;
+    this.detail = detail;
+    this.vertices = Torus.getVertices(R, r, detail);
   }
 
-  return vertices;
+  static getVertices(R, r, detail = 32) {
+    // empty list
+    let vertices = [];
+    // iterating through all the thetas
+    for (let radian1 = 0; radian1 <= Math.PI * 2; radian1 += Math.PI * 2 / detail) {
+      // iterating through all the phis
+      for (let radian2 = 0; radian2 <= Math.PI * 2; radian2 += Math.PI * 2 / detail * 10) {
+        // appending list with a vector
+        vertices.push(Torus.getVector(radian1, radian2));
+      }
+    }
+    return vertices;
+  }
 
-  function getVertex(theta, phi) {
+  static getVector(theta, phi) {
     // https://en.wikipedia.org/wiki/Torus#Geometry
-    return {
-      x: ((R + (r * Math.cos(theta))) * Math.cos(phi)).toFixed(6),
-      y: ((R + (r * Math.cos(theta))) * Math.sin(phi)).toFixed(6),
-      z: (r * Math.sin(theta)).toFixed(6)
-    };
+    return [
+      /* x */ ((R + (r * Math.cos(theta))) * Math.cos(phi)).toFixed(6),
+      /* y */ ((R + (r * Math.cos(theta))) * Math.sin(phi)).toFixed(6),
+      /* z */ (r * Math.sin(theta)).toFixed(6)
+    ];
   }
 }
