c82485339dc0646304cda6224e74080608b50c8d
Ethan <ethan.r.davidson@gmail.com>
2018-08-02 16:12:13 -0700

    Linted my js files

--- examples/threejs-custom-shape/scripts/sketch.js
+++ examples/threejs-custom-shape/scripts/sketch.js
@@ -10,28 +10,30 @@ let currentShape = "heart";
 
 function init() {
   camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.01, 10);
-	camera.position.z = 1;
+  camera.position.z = 1;
 
-	scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
   heart = new Heart(0.02);
 
-	geometry = heart.geometry;
-	material = new THREE.MeshNormalMaterial();
-	mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  geometry = heart.geometry;
+  material = new THREE.MeshNormalMaterial();
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setSize(WIDTH, HEIGHT);
-	$("#cnv-container").append(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(WIDTH, HEIGHT);
+  $("#cnv-container").append(renderer.domElement);
 
   orbit = new THREE.OrbitControls(camera, renderer.domElement);
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+  requestAnimationFrame(animate);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
   mesh.rotation.z += 0.02;
   renderer.render(scene, camera);
 }
@@ -44,7 +46,8 @@ function changeShape() {
     case "heart":
       mesh = new THREE.Mesh(new Heart(0.02).geometry, material);
       break;
-    default: break;
+    default:
+      break;
   }
   scene.add(mesh);
 }
--- examples/threejs-test/scripts/sketch.js
+++ examples/threejs-test/scripts/sketch.js
@@ -9,27 +9,29 @@ let geometry, material, mesh;
 
 function init() {
   camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.01, 10);
-	camera.position.z = 1;
+  camera.position.z = 1;
 
-	scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-	material = new THREE.MeshNormalMaterial();
+  geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  material = new THREE.MeshNormalMaterial();
 
-	mesh = new THREE.Mesh(geometry, material);
-	scene.add(mesh);
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.setSize(WIDTH, HEIGHT);
-	$("#cnv-container").append(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+  renderer.setSize(WIDTH, HEIGHT);
+  $("#cnv-container").append(renderer.domElement);
 
   orbit = new THREE.OrbitControls(camera, renderer.domElement);
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+  requestAnimationFrame(animate);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
   mesh.rotation.z += 0.02;
   renderer.render(scene, camera);
 }
@@ -74,7 +76,8 @@ function changeShape() {
     case "torus knot":
       mesh = new THREE.Mesh(new THREE.TorusKnotGeometry(0.3, 0.1, 100, 16), material);
       break;
-    default: /* PlaneGeometry */;
+    default:
+      /* PlaneGeometry */ ;
   }
   scene.add(mesh);
 }
