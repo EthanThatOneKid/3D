5b7880e00aa30e62eb318c1f527bbeba51ecc639
Ethan <ethan.r.davidson@gmail.com>
2018-08-21 20:26:53 -0700

    Beautified and added a comment

--- examples/threejs-hypercube/scripts/hypercube.js
+++ examples/threejs-hypercube/scripts/hypercube.js
@@ -1,5 +1,4 @@
 class Hypercube {
-
   constructor(r) {
     this.r = r;
     this.rotation = {x: 0, y: 0, z: 0, w: 0};
@@ -12,12 +11,13 @@ class Hypercube {
           let gimmeZ = [];
           for (let w = 0; w < 2; w++) {
             let vertex = [(x - 0.5) * r, (y - 0.5) * r, (z - 0.5) * r, (w - 0.5) * r];
-            gimmeZ.push(tf.tensor(vertex));
+            gimmeZ.push(vertex);
           }
           gimmeY.push(gimmeZ);
         } gimmeX.push(gimmeY);
       } this.data.push(gimmeX);
     }
+    // Connections are arbitrarily ordered.
     this.connections = [[9,8],[9,1],[9,11],[9,13],[5,4],[5,7],[5,13],[5,1],[3,2],[3,11],[3,7],[3,1],[15,14],[15,11],[15,7],[15,13],[0,8],[0,2],[0,4],[0,1],[10,11],[10,8],[10,2],[10,14],[12,14],[12,13],[12,4],[12,8],[6,7],[6,14],[6,4],[6,2]];
   }
   project3d(xAngle = 0, yAngle = 0, zAngle = 0) {
@@ -26,21 +26,21 @@ class Hypercube {
       for (let y = 0; y < this.data[x].length; y++) {
         for (let z = 0; z < this.data[x][y].length; z++) {
           for (let w = 0; w < this.data[x][y][z].length; w++) {
-            let s = Math.sin(xAngle), c = Math.cos(xAngle), gimmeProjectionData;
-            tf.tidy(() => {
-              const v = tf.tensor([[...this.data[x][y][z][w].dataSync()]]).transpose();
-              let double_rot = tf.tensor([[c,-s,0,0],[s,c,0,0],[0,0,c,-s],[0,0,s,c]]);
-              let zw_rot = tf.tensor([[1,0,0,0],[0,1,0,0],[0,0,c,-s],[0,0,s,c]]);
-              let xw_rot = tf.tensor([[c,0,0,-s],[0,1,0,0],[0,0,1,0],[s,0,0,c]]);
-              let zy_rot = tf.tensor([[1,0,0,0],[0,c,0,-s],[0,0,1,0],[0,s,0,c]]);
-              let rotated = tf.matMul(double_rot, v);
-              let temp = rotated.dataSync(), temp_w = this.r / (this.r - temp[3]);
-              let projection = tf.tensor([
+            const s = Math.sin(xAngle), c = Math.cos(xAngle);
+            const gimmeProjectionData = tf.tidy(() => {
+              const v = tf.tensor([this.data[x][y][z][w]]).transpose();
+              const double_rot = tf.tensor([[c,-s,0,0],[s,c,0,0],[0,0,c,-s],[0,0,s,c]]);
+              // const zw_rot = tf.tensor([[1,0,0,0],[0,1,0,0],[0,0,c,-s],[0,0,s,c]]);
+              // const xw_rot = tf.tensor([[c,0,0,-s],[0,1,0,0],[0,0,1,0],[s,0,0,c]]);
+              // const zy_rot = tf.tensor([[1,0,0,0],[0,c,0,-s],[0,0,1,0],[0,s,0,c]]);
+              const rotated = tf.matMul(double_rot, v);
+              const temp = rotated.dataSync(), temp_w = this.r / (this.r - temp[3]);
+              const projection = tf.tensor([
                 /* x */ temp[0] / temp_w,
                 /* y */ temp[1] / temp_w,
                 /* z */ temp[2] / temp_w
               ]).mul(tf.scalar(this.r * 10));
-              gimmeProjectionData = projection.dataSync();
+              return projection.dataSync();
             });
             result.push(gimmeProjectionData);
           }
